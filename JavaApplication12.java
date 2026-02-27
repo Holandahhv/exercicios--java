d16c7a3dcb57b34ed2aa8f890417553310cb5be3
Holandahhv <pedrohhv30@gmail.com>
2026-02-27 19:01:53 -0300

    Add files via upload

---
+++ JavaApplication12.java
@@ -0,0 +1,14 @@
+package javaapplication12;
+
+public class JavaApplication12 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        
+        while(contador < 20){
+            System.out.println("contador:" + contador);
+            contador++;
+        }
+    }
+    
+}
